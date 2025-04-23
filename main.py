7cfd4ebce7131e58546b0c57a8e0efb812460170
Gregor <a52981@alunos.ipb.pt>
2025-04-24 00:39:35 +0100

    Criação do repositório

---
+++ main.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+DEVICES = ['raidz2_sync', 'raidz2_async', 'raid6_xfs']
+MODES = ['bw', 'iops', 'lat']
+
+def get_images(rw, blocksize, filter,machine, type):
+    lista_imagens = []
+    for device in DEVICES:
+        for mode in MODES:
+            if rw == 'randrw':
+                lista_imagens.append(f"benchs/{machine}/{type}/{device}/{rw}/{blocksize}/{device}_{rw}_{filter}_{blocksize}_{mode}.png")
+            else:
+                lista_imagens.append(f"benchs/{machine}/{type}/{device}/{blocksize}/{device}_{rw}_{blocksize}_{mode}.png")
+    return lista_imagens
+
+st.set_page_config(layout="wide")
+st.title("Grelha Dinâmica de Imagens")
+
+with st.sidebar:
+    machine = st.selectbox("Escolha a máquina", ['83', '84'])
+    all_io = ['sata-onboard', 'sata-sas', 'sas-sas']
+    io_options = [io for io in all_io if not (machine == '84' and io == 'sata-onboard')]
+    io_type = st.selectbox("Escolha o controlador I/O", io_options)
+    rw = st.selectbox("Escolha o rw", ["randread", "randwrite", "randrw"])
+    blocksize = st.selectbox("Escolha o blocksize", ["1M", "512k", "4k"])
+    filter_type = None
+    if rw == 'randrw': 
+        filter_type = st.selectbox("Escolha o filter", ["read", "write"])
+
+st.write(
+    f"**Opções selecionadas:** MV={machine}, IO={io_type}, RW={rw}, "
+    f"BS={blocksize}, FT={filter_type if filter_type else 'N/A'}"
+)
+images = get_images(rw, blocksize, filter_type, machine, io_type)
+col_titles = ["Bandwidth", "IOPS", "Latência"]
+row_titles = ["ZFS Sync", "ZFS Async", "XFS"]
+
+for row_idx, row_title in enumerate(row_titles):
+    st.markdown(f"### {row_title}")
+    cols = st.columns(3)
+    for col_idx, col_title in enumerate(col_titles):
+        with cols[col_idx]:
+            st.image(images[row_idx * 3 + col_idx], caption=col_title, width=300, use_container_width=True)
+
+st.title("Melhores dos Melhores")
+melhores_imagens = []
+melhores_imagens.append(f"benchs/83/sata-onboard/raidz2_async/4k/raidz2_async_randread_4k_iops.png")
+melhores_imagens.append(f"benchs/83/sata-onboard/raidz2_async/1M/raidz2_async_randread_1M_bw.png")
+melhores_imagens.append(f"benchs/83/sata-onboard/raidz2_async/4k/raidz2_async_randread_4k_lat.png")
+best_iops = melhores_imagens[0]  
+best_bw = melhores_imagens[1]    
+best_lat = melhores_imagens[2]   
+
+st.markdown("### Melhor IOPS")
+st.image(best_iops, caption="Melhor IOPS", width=300, use_container_width=True)
+
+st.markdown("### Melhor Bandwidth")
+st.image(best_bw, caption="Melhor Bandwidth", width=300, use_container_width=True)
+
+st.markdown("### Melhor Latência")
+st.image(best_lat, caption="Melhor Latência", width=300, use_container_width=True)
+
+st.write("G.U. - J.R. - P.D // 2025 // v1.2")
